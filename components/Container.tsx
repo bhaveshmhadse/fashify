c8e01364483447fbae29362592ad96f137606e75
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-11-01 16:06:22 +0530

    Refactored

--- components/Container.tsx
+++ components/Container.tsx
@@ -85,19 +85,26 @@ const Container = ({}) => {
     }, 0);
   };
 
+  let [showDown, setshowDown] = useState(true);
+
   useEffect(() => {
     setuserSelection(previousSelection => ({ ...previousSelection, ...getItemFromLocalStorage("userSelection") }));
     setTimeout(() => {
       // reloadClothes()
-      console.log('userSelection is:', userSelection)
-      
+      console.log("userSelection is:", userSelection);
     }, 2000);
   }, []);
 
   return (
     <div className='w-full h-auto lg:h-full flex flex-col items-center justify-center m-0 bg-slate-100 relative'>
-      <button onClick={() => alertSomething({ outfitColour, userSelection })} className='fixed'>
+      {/* <button onClick={() => alertSomething({ outfitColour, userSelection })} className='fixed'>
         Baray
+      </button> */}
+      <button className='fixed top-52' onClick={() => localStorage.clear()}>
+        Reset Local
+      </button>
+      <button className='fixed top-64' onClick={() => setshowDown(prev => !prev)}>
+        Hide it
       </button>
       <div className='w-full h-full lg:py-12 lg:items-center lg:justify-center lg:flex' style={{ width: "100vw", height: "100vh", margin: "0" }}>
         <div className='h-full lg:flex lg:flex-row flex flex-col w-full lg:w-11/12 lg:items-center lg:justify-center lg:px-6'>
@@ -130,22 +137,23 @@ const Container = ({}) => {
             </div>
           </div>
           <div className='h-1/6 mt-10 z-0 flex items-center overflow-y-auto  lg:hidden lg:w-2/6 lg:grid-cols-2 lg:h-5/6 lg:overflow-y-auto gap-4 px-2 '></div>
-          <div className='h-auto pb-2 fixed w-full lg:relative z-20 bottom-0  flex items-center  overflow-y-auto  lg:grid lg:w-2/6 lg:grid-cols-2 lg:h-5/6 lg:overflow-y-auto gap-4 px-2 '>
-            {Object.keys(genderOutfits[userSelection.gender]).map(eachFashionPart => {
-              if (eachFashionPart == "items") return;
-              return (
-                <div key={Math.random().toString()} onClick={() => handleClothesSelectionModal(eachFashionPart)} className={`w-auto lg:py-8 lg:my-2 px-12 mx-1 h-3/4 rounded-lg items-center justify-center flex font-black hover:border-0 text-sm lg:w-full lg:m-0 lg:p-0 item lg:h-full whitespace-nowrap hover:bg-blue-200 hover:border-blue-700 hover:text-blue-700 duration-300 ${userSelection[currentSelectTedFashionPart] == eachFashionPart ? " bg-blue-200 border-blue-700 text-blue-700 " : " rounded-xl  border-gray-300 "} flex-col`}>
-                  {/* <div key={Math.random().toString()} onClick={() => handleClothesSelectionModal(eachFashionPart)} className=' px-8  h-3/4 items-center justify-center flex font-black text-xl lg:w-full lg:h-full lg:m-0 lg:p-0 items rounded-xl border-1 border-gray-300'>
+          {showDown && (
+            <div className='h-auto pb-2 fixed w-full lg:relative z-20 bottom-0  flex items-center  overflow-y-auto  lg:grid lg:w-2/6 lg:grid-cols-2 lg:h-5/6 lg:overflow-y-auto gap-4 px-2 '>
+              {Object.keys(genderOutfits[userSelection.gender]).map(eachFashionPart => {
+                if (eachFashionPart == "items") return;
+                return (
+                  <div key={Math.random().toString()} onClick={() => handleClothesSelectionModal(eachFashionPart)} className={`w-auto lg:py-8 lg:my-2 px-12 mx-1 h-3/4 rounded-lg items-center justify-center flex font-black hover:border-0 text-sm lg:w-full lg:m-0 lg:p-0 item lg:h-full whitespace-nowrap hover:bg-blue-200 hover:border-blue-700 hover:text-blue-700 duration-300 ${userSelection[currentSelectTedFashionPart] == eachFashionPart ? " bg-blue-200 border-blue-700 text-blue-700 " : " rounded-xl  border-gray-300 "} flex-col`}>
+                    {/* <div key={Math.random().toString()} onClick={() => handleClothesSelectionModal(eachFashionPart)} className=' px-8  h-3/4 items-center justify-center flex font-black text-xl lg:w-full lg:h-full lg:m-0 lg:p-0 items rounded-xl border-1 border-gray-300'>
                   {eachFashionPart}
                   <img src={`${eachFashionPart}.png`} alt='' />
                 </div> */}
-                  <img className='w-16 h-auto icon ' src={`${eachFashionPart.toString().toLowerCase().split(" ").join("-")}.svg`} alt='' />
-                  <div className='text-gray-600 m-auto'>{eachFashionPart}</div>
-                </div>
-              );
-            })}
-          </div>
-
+                    <img className='w-16 h-auto icon ' src={`${eachFashionPart.toString().toLowerCase().split(" ").join("-")}.svg`} alt='' />
+                    <div className='text-gray-600 m-auto'>{eachFashionPart}</div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
           {showClothTypeSelection && (
             <div className='flex fixed z-30 bottom-0 left-0 right-0 items-end lg:items-center justify-end h-1/6 p-0 rounded-t-xl rounded-b-none lg:right-0 lg:top-0 lg:ml-auto  lg:bottom-0 lg:w-1/6 lg:h-full lg:rounded-r-xl rounded-xl border-t4 lg:border-l4 border-slate-300 bg-blue-50 shadow-lg shadow-black'>
               <button className='flex items-center justify-center absolute top-3 right-3 font-black text-black lg:left-3 lg:top-3 lg:justify-start' onClick={() => setShowClothTypeSelection(false)}>
