a0802661a9260213cabe1356e8756e85d487dd5d
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-11-01 16:16:43 +0530

    Refactored

--- components/Container.tsx
+++ components/Container.tsx
@@ -95,7 +95,7 @@ const Container = ({}) => {
     }, 2000);
   }, []);
 
-  return <div className=' w-full h-auto lg:h-full flex flex-col items-center justify-center m-0 bg-slate-100 relative'></div>;
+  return <div className=' w-full h-auto lg:h-full flex flex-col items-center justify-center m-0 bg-slate-100 relative'>Baray</div>;
 };
 
 export default Container;
